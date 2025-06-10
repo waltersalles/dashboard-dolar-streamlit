0d93679e6eae31ca39aef86372707c4233ed6ce8
waltersalles <61155133+waltersalles@users.noreply.github.com>
2025-06-10 20:55:38 -0300

    código_do_dashboard_do_dólar.py

--- código_do_dashboard_do_dólar.py
+++ código_do_dashboard_do_dólar.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1w5-TQGrheVUzRrt4M12E8eDgPm75nO7e
 """
 
-pip install streamlit pandas plotly yfinance
-
 # dashboard_dolar.py
 
 import streamlit as st
